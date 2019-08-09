fcef1d551a3d5fd2a97a8ad052ff67829e035b3d
Andre Antunes Vieira <andreantunesv@gmail.com>
2019-08-09 13:47:00 -0300

    v0.0.2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs=require("fs"),transformExpected=expected=>{if(""===expected)return"''";const parse1=expected.replace(/\/\/.*/,"").trim();return"{"===parse1.substr(0,1)?JSON.parse(parse1.replace(/\n/g,"")):parse1.match(/^['"](.*)['"]$/)?parse1.replace(/'(.*)'/,"$1"):eval(parse1)};void 0===global.StoryBookSections&&(global.StoryBookSections=[]),void 0===global.StoryBookSectionCounter&&(global.StoryBookSectionCounter=0);const parseArgs=t=>"function"!=typeof t[1]?t:[t[0],"",t[1]],historyCreator=mainTitle=>{return global.StoryBookSections.push(`### ${mainTitle}
+const fs=require("fs"),transformExpected=expected=>{if(""===expected)return"''";const parse1=expected.replace(/\/\/.*/,"").trim();return"{"===parse1.substr(0,1)?JSON.parse(parse1.replace(/\n/g,"")):parse1.match(/^['"](.*)['"]$/)?parse1.replace(/'(.*)'/,"$1"):eval(parse1)};void 0===global.StoryBookSections&&(global.StoryBookSections=[]),void 0===global.StoryBookSectionCounter&&(global.StoryBookSectionCounter=0);const parseArgs=t=>"function"!=typeof t[1]?t:[t[0],"",t[1]],storyCreator=mainTitle=>{return global.StoryBookSections.push(`### ${mainTitle}
 
 
 `),(...args)=>{const[testTitle,subtitle,callback]=parseArgs(args);let allHistory=`#### ${testTitle}
@@ -10,4 +10,4 @@ const fs=require("fs"),transformExpected=expected=>{if(""===expected)return"''";
 `,timeout=setTimeout(()=>{if(testHistory+="```",allHistory+=`${testHistory}
 `,0===--testCounter&&(global.StoryBookSections.push(allHistory),0===--global.StoryBookSectionCounter)){const t=fs.readFileSync("README.md","utf8"),e=global.StoryBookSections.join("").replace(/\n/g,"å");let o=t.replace(/\n/g,"å");o.match(new RegExp(`### ${mainTitle}.*å### `))?o=o.replace(new RegExp(`### ${mainTitle}.*å### `),`${e}å### `):o.match(new RegExp(`### ${mainTitle}.*`))?o=o.replace(new RegExp(`### ${mainTitle}.*`),`${e}å`):o+=`${e}å`,fs.writeFileSync("README.md",o.replace(/å/g,"\n"))}resolve()},100)};callback(sample)})})};describe(mainTitle,()=>callback(variation))}};
 
-module.exports = {historyCreator}
+module.exports = {storyCreator}
